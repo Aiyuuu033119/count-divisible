b2c6a5fac9ff802b37c954294db423641f3c4758
unknown <idestura35@gmail.com>
2021-03-09 13:32:39 +0800

    try

--- index.php
+++ index.php
@@ -14,23 +14,23 @@
         public function index($x,$y,$p)
         {
             echo('running');
-            echo $x; //returns codex
-            echo $y; //returns foo
-            echo $p; //returns bar
+            // echo $x; //returns codex
+            // echo $y; //returns foo
+            // echo $p; //returns bar
 
-            // $data = array(
-            //     'x' => $x,
-            //     'y' => $y,
-            //     'p' => $p
-            // );
+            $data = array(
+                'x' => $x,
+                'y' => $y,
+                'p' => $p
+            );
 
-            // if ($array['x'] % $array['p'] == 0){
-            //     $info = $array['y'] / $array['p'] - $array['x'] / $array['p'] + 1;
-            // }
+            if ($array['x'] % $array['p'] == 0){
+                $info = $array['y'] / $array['p'] - $array['x'] / $array['p'] + 1;
+            }
 
-            // $info = $array['y'] / $array['p'] - $array['x'] / $array['p'];
+            $info = $array['y'] / $array['p'] - $array['x'] / $array['p'];
 
-            // echo json_encode($info);
+            echo json_encode($info);
         }
     }
 
