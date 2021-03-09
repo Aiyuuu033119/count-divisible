7ff39a3ca1ed3014a63d7d98a861eaf80894e427
unknown <idestura35@gmail.com>
2021-03-09 12:23:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+    class Compute{
+        
+        public function divisible()
+        {
+            $x = $_POST['x'];
+            $y = $_POST['y'];
+            $p = $_POST['p'];
+
+            $data = array(
+                'x' => $x,
+                'y' => $y,
+                'p' => $p
+            );
+
+            $index = new Compute();
+            $info = $index->result($data);
+            
+            echo json_encode($info);
+        }
+
+        public function result($array) {	
+            if ($array['x'] % $array['p'] == 0){
+                return( $array['y'] / $array['p'] - $array['x'] / $array['p'] + 1);
+            }
+            return($array['y'] / $array['p'] - $array['x'] / $array['p']);
+        }
+    }
+    
+
+?>
