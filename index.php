cdd682b56dc6581387bd17a4127d7d7effb41648
unknown <idestura35@gmail.com>
2021-03-09 13:59:25 +0800

    remove

--- index.php
+++ index.php
@@ -5,30 +5,22 @@
     $uri = $_SERVER['REQUEST_URI'];
     $uri = trim($uri, '/');
     
-    $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    
     $index = new compute();
-    $index->index($uriSegments[2],$uriSegments[3],$uriSegments[4]);
+    $index->index(intval($_GET['x']),intval($_GET['y']),intval($_GET['p']));
 
     class compute{
         public function index($x,$y,$p)
         {
-            echo('running');
+            // echo('running');
             // echo $x; //returns codex
             // echo $y; //returns foo
             // echo $p; //returns bar
 
-            $data = array(
-                'x' => $x,
-                'y' => $y,
-                'p' => $p
-            );
-
-            if ($array['x'] % $array['p'] == 0){
-                $info = $array['y'] / $array['p'] - $array['x'] / $array['p'] + 1;
+            if ($x % $p == 0){
+                $info = $y / $p - $x / $p + 1;
             }
 
-            $info = $array['y'] / $array['p'] - $array['x'] / $array['p'];
+            $info = $y / $p - $x / $p;
 
             echo json_encode($info);
         }
